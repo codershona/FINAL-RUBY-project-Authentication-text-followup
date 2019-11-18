22cb98fcc3ee42559c3b8b9967067af2c25a4bf0
falguniislam <islamfalguni12@gmail.com>
2019-11-18 18:46:54 +0100

    working on comparing passes

--- Final rubyproject- comparing passwords/crud.rb
+++ Final rubyproject- comparing passwords/crud.rb
@@ -6,3 +6,30 @@ gemfile true do
 end
 
 require 'bcrypt'
+
+users = [
+    { username: "falguni", password: "995577" },
+    { username: "jack", password: "9955" },
+    { username: "shaim", password: "9944" },
+    { username: "ali", password: "9911" },
+   { username: "mehnaz", password: "9922" }
+]
+def create_hash_digest(password)
+  BCrypt::Password.create(password)
+end
+
+def verify_hash_digest(passwords)
+  BCrypt::Password.new(password)
+end
+
+def create_secure_users(list_of_users)
+  list_of_users.each do |user_record|
+     user_record[:password] = create_hash_digest(user_record[:password])
+  end
+  list_of_users
+end 
+
+puts create_secure_users(users)
+
+# new_password = create_hash_digest("password1")
+# puts new_password =="password2"
