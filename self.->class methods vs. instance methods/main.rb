e80eba147418a8352ed119617086f58cb84d8493
falguniislam <islamfalguni12@gmail.com>
2019-11-18 19:12:17 +0100

    working on self methods in ruby

---
+++ self.->class methods vs. instance methods/main.rb
@@ -0,0 +1,12 @@
+require_relative 'crud'
+
+users = [
+     { username: "falguni", password: "995577" },
+     { username: "jack", password: "9955" },
+     { username: "shaim", password: "9944" },
+     { username: "ali", password: "9933" },
+    { username: "mehnaz", password: "9922" }
+]
+
+hashed_users = Crud.create_secure_users(users)
+puts hashed_users
